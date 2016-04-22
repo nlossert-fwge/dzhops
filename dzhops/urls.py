24d885c5cd402d8470651dddff2fa6ae70eb8ed5
nlossert-fwge <admin@fwge.net>
2016-04-22 11:20:09 +0800

    Update urls.py
    去掉common

--- dzhops/urls.py
+++ dzhops/urls.py
@@ -7,10 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dzhops.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    # url(r'^common/', include('common.urls')),
     url(r'^$', 'index.views.index', name='index'),
     url(r'^accounts/', include('index.urls')),
-    url(r'^common/', include('common.urls')),
     url(r'^salt/', include('saltstack.urls')),
     url(r'^hostlist/', include('hostlist.urls')),
     url(r'^data/', include('replacedata.urls')),
